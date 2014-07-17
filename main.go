8317a19a12346792a838ac3495269469ffea1ec3
Nikita Loskutov <cnaize@gmail.com>
2014-07-17 08:56:23 +0400

    added error handling

--- main.go
+++ main.go
@@ -15,13 +15,17 @@ func main() {
 	root_path, err := osext.Executable()
 	if err != nil {
 		fmt.Printf("Error: %v", err)
+		return
 	}
 	template_path := root_path + "/templates/" + "test"
 	text_path := template_path + ".txt"
 
+	fmt.Printf("Is file exists: %v\n", exist(text_path))
+
 	textTmpl, err := template.ParseFiles(text_path)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
+		return
 	}
 
 	var text bytes.Buffer
@@ -30,6 +34,7 @@ func main() {
 
 	if err := textTmpl.ExecuteTemplate(&text, "test"+".txt", args); err != nil {
 		fmt.Printf("Error: %v", err)
+		return
 	}
 
 	fmt.Println("End")
